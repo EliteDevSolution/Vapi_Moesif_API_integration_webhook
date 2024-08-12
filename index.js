3ac66783b32bd1fbd13fc42def17c2bf79c7294f
BarosJiang <barosjiang@gmail.com>
2024-08-12 11:49:06 +0300

    Update: VAPI & Stripe & Moesif the whole

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 require('dotenv').config();
@@ -8,6 +7,9 @@ const app = express().use(bodyParser.json()); // creates http server
 
 const token = process.env.VAPI_TOKEN;
 const applicationId = process.env.MOESIF_APP_ID;
+const vapiApiUrl = 'https://api.vapi.ai/call';
+const vapiOutboundUrl = 'https://api.vapi.ai/call/phone';
+const stripeMeterApiUrl = 'https://api.stripe.com/v1/billing/meter_events';
 const moesifApiUrl = 'https://api.moesif.net/v1/actions';
 
 app.get('/', (req, res) => {
@@ -15,7 +17,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-    //console.log("first", req.body);
     // check if verification token is correct
     const vapiSecretToken = req.headers['x-vapi-secret'];
 
@@ -27,9 +28,8 @@ app.post('/webhook', (req, res) => {
     if(callStatus === "end-of-call-report")
     {
         var callID = req.body.message.call.id || '';
-        //callID = "fa61c90a-4455-4671-8902-3393c89419b3";
-
-        fetch(`https://api.vapi.ai/call/${callID}`, {
+        //callID = "c9ea9b10-f5ab-4c68-89e2-e859aaca2182";
+        fetch(`${vapiApiUrl}/${callID}`, {
             method: 'get',
             headers: new Headers({
                 'Authorization': `Bearer ${process.env.VAPI_PRIVATE_KEY}`,
@@ -43,48 +43,74 @@ app.post('/webhook', (req, res) => {
             const endTime = resObj.endedAt;
             const duration = getDuration(startTime, endTime);
             const cost =  resObj.cost.toFixed(2);
-            const phoneNumber = req.body.customer?.number || '';
+            const phoneNumber = resObj?.customer?.number || '';
+            const customer_email = resObj?.customer?.name || '';
+            const customer_id = resObj?.metadata?.customerId || '';
+            const subscription_id = resObj?.metadata?.subscriptionId || '';
 
             const data = {
               action_name: 'VAPI CALL Ended',
-              company_id: "cus_QX8yUwzGuAu12a",
+              company_id: customer_id,
+              subscription_id: subscription_id,
               request: {
                 time: new Date().toISOString()
-
               },
               metadata: {
                 assistant_id: assistantId,
-                customer_id: '123453829',
+                customer_id: customer_id,
+                customer_email: customer_email,
                 call_id: callID,
                 phone_number: phoneNumber,
                 cost: cost,
                 duration_minutes: duration.minutes,
                 duration_seconds: duration.seconds,
+                duration_total_minutes: duration.total_minutes
               }
             };
 
-            const headers = {
-              'Content-Type': 'application/json',
-              'X-Moesif-Application-Id': applicationId
+            const stripeApiHeader =  {
+              'Authorization': `Bearer ${process.env.STRIPE_KEY}`,
+              'Content-Type': 'application/x-www-form-urlencoded',
             };
 
-            fetch(moesifApiUrl, {
+            const urlencoded = new URLSearchParams();
+            urlencoded.append("event_name", process.env.STRIPE_METER_NAME);
+            urlencoded.append("payload[stripe_customer_id]", customer_id);
+            urlencoded.append("payload[value]", duration.total_minutes);
+
+            fetch(stripeMeterApiUrl, {
               method: 'POST',
-              headers: headers,
-              body: JSON.stringify(data)
+              headers: stripeApiHeader,
+              body: urlencoded
             })
-            .then(response => {
-              if (response.ok) {
-                return res.json({
-                  status: true,
+            .then(res => res.json())
+            .then(resObj => {
+              const headers = {
+                'Content-Type': 'application/json',
+                'X-Moesif-Application-Id': applicationId
+              };
+
+              fetch(moesifApiUrl, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(data)
+              })
+              .then(response => {
+                if (response.ok) {
+                  return res.json({
+                    status: true,
+                });
+                  console.log('Moesif API Request successful');
+                } else {
+                  return res.json({
+                    status: false,
+                });
+                  console.error('Moesif API Request failed:', response.status);
+                }
+              })
+              .catch(error => {
+                console.error('Error:', error);
               });
-                console.log('Moesif API Request successful');
-              } else {
-                return res.json({
-                  status: false,
-              });
-                console.error('Moesif API Request failed:', response.status);
-              }
             })
             .catch(error => {
               console.error('Error:', error);
@@ -92,6 +118,49 @@ app.post('/webhook', (req, res) => {
         })
         .catch(error => console.error('VAPI API Calling Error: ', error));
     }
+});
+
+app.post('/outbound', async (req, res) => {
+  // Extract data from the request body
+  const { phoneNumberId, assistantId, customerId, subScriptionId, customerEmail, customerNumber } = req.body;
+  try {
+    // Make a POST request to the Vapi API
+    const response = await fetch(
+      vapiOutboundUrl,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.VAPI_PRIVATE_KEY}`,
+        },
+        body: JSON.stringify({
+          phoneNumberId,
+          assistantId,
+          customer: {
+            name: customerEmail,
+            number: customerNumber
+          },
+          metadata: {
+            customerId: customerId,
+            subscriptionId: subScriptionId
+          }
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+
+    // Parse the response data as JSON
+    const data = await response.json();
+
+    // Send the response data as JSON
+    res.status(200).json(data);
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ message: 'Failed to place outbound call', error: error.message });
+  }
 });
 
 function getDuration(startTime = '', endTime = '')
@@ -106,15 +175,14 @@ function getDuration(startTime = '', endTime = '')
     // Convert the duration to seconds
     const durationSeconds = durationMs / 1000;
     // Calculate minutes and seconds
-    const minutes = Math.floor(durationSeconds / 60);
-    const seconds = Math.round(durationSeconds % 60);
-    if(minutes === 0  && seconds > 0)
+    let minutes = Math.floor(durationSeconds / 60);
+    let seconds = Math.round(durationSeconds % 60);
+    let total_minutes = minutes;
+    if(seconds > 0)
     {
-      minutes = 1;
-      seconds = 0;
+      total_minutes++;
     }
-    return {minutes, seconds}
+    return {minutes, seconds, total_minutes}
 }
 
-
-app.listen(3000, () => console.log('VAPI Webhook is listening'));
+export default app;
